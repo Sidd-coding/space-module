5653e3dd80e9cef74698bd82a3df2373ba1d25a2
Sidd-coding <rahulsrivastavasidd@gmail.com>
2021-07-23 16:56:15 +0530

    update header

--- src/Header.js
+++ src/Header.js
@@ -3,13 +3,15 @@ import logo from './logo/spacex-logo2.jpg';
 
 function Header() {
     return (
-        <div className="space">
-            <img src={logo}
-                alt="logo"
-                height="55px"
-                width="500px"
-            >
-            </img>
+        <div className="app_bar">
+            <div className="img_container container">
+                <img src={logo}
+                    alt="logo"
+                    height="55px"
+                    width="500px"
+                >
+                </img>
+            </div>
         </div>
     )
 };
